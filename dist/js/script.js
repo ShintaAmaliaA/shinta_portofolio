0176d987570ccaa74ac0eb68e5430b33fb36e48d
Shinta <shintaamaliaazhara@gmail.com>
2026-05-23 22:17:18 +0700

    ..

--- dist/js/script.js
+++ dist/js/script.js
@@ -46,9 +46,98 @@ darkToggle.addEventListener('click', function () {
   }
 });
 
-// pindahkan posisi toggle sesuai mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  darkToggle.checked = true;
-} else {
-  darkToggle.checked = false;
-}
+  document.addEventListener("click", function(e) {
+
+    // bunga random
+    const flowers = ["🌸", "🌷", "🌺", "💮", "🌼"];
+
+    for(let i = 0; i < 8; i++) {
+
+      const flower = document.createElement("span");
+      flower.classList.add("flower-click");
+
+      flower.innerHTML =
+        flowers[Math.floor(Math.random() * flowers.length)];
+
+      // posisi klik
+      flower.style.left = e.clientX + "px";
+      flower.style.top = e.clientY + "px";
+
+      // ukuran random
+      flower.style.fontSize =
+        (Math.random() * 20 + 15) + "px";
+
+      // arah random
+      const x = (Math.random() - 0.5) * 150;
+      const y = (Math.random() - 0.5) * 150;
+
+      flower.animate([
+        {
+          transform: "translate(0,0) scale(0.5)",
+          opacity: 1
+        },
+        {
+          transform: `translate(${x}px, ${y}px) scale(1.5)`,
+          opacity: 0
+        }
+      ], {
+        duration: 2000,
+        easing: "ease-out"
+      });
+
+      document.body.appendChild(flower);
+
+      setTimeout(() => {
+        flower.remove();
+      }, 2000);
+    }
+
+  });
+
+   const helloText = document.getElementById("hello-text");
+
+    const greetings = [
+      "Hello",
+      "こんにちは",
+      "안녕하세요",
+      "Halo",
+      "Bonjour",
+      "Hola",
+      "Ciao",
+      "مرحبا",
+      "你好",
+      "Привет"
+    ];
+
+    let index = 0;
+
+    const changeText = setInterval(() => {
+
+      helloText.style.opacity = 0;
+      helloText.style.transform = "scale(0.9)";
+
+      setTimeout(() => {
+
+        index++;
+
+        if(index >= greetings.length){
+          clearInterval(changeText);
+          return;
+        }
+
+        helloText.innerHTML = greetings[index];
+
+        helloText.style.opacity = 1;
+        helloText.style.transform = "scale(1)";
+
+      }, 100);
+
+    }, 200);
+
+  
+    // pindahkan posisi toggle sesuai mode
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
